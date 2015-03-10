4dcea47a70436b116b83ada0490232e731b780da
McLeanBH <mcleanbh@gmail.com>
2015-03-10 17:00:39 -0400

    content security policy updates

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,7 @@ module.exports = function(environment) {
      'default-src': "'none'",
      'script-src': "'self'",
      'font-src': "'self'",
-     'connect-src': "'self' http://api.parse.com",
+     'connect-src': "http://api.parse.com",
      'img-src': "'self'",
      'style-src': "'self' 'unsafe-inline'", //
      'media-src': "'self'"

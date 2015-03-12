baafce503c1573c05748bd3bd4ee4c75f497e86e
McLeanBH <mcleanbh@gmail.com>
2015-03-12 11:01:46 -0400

    updated user.js adapter

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,4 +1,18 @@
-// app/adapters/users.js //
+import ajax from 'ic-ajax';
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+  find: function(name, id){
+    /* jshint unused: false */
+    return ajax("https://api.parse.com/1/users/" + id).then(function(user){
+      user.id = user.objectId;
+      delete user.objectId;
+      delete user.sessionToken;
+      return user;
+    });
+  }
+});
+
 
 // import ajax from 'ic-ajax';
 // import Ember from 'ember';
@@ -66,20 +80,3 @@
 //     }
 //   }
 // });
-
-
-
-import ajax from 'ic-ajax';
-import Ember from 'ember';
-
-export default Ember.Object.extend({
-  find: function(name, id){
-    /* jshint unused: false */
-    return ajax("https://api.parse.com/1/users/" + id).then(function(user){
-      user.id = user.objectId;
-      delete user.objectId;
-      delete user.sessionToken;
-      return user;
-    });
-  }
-});

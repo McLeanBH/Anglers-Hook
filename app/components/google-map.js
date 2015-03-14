5b7f468f16a1da7706b79894cb94027083e865ea
McLeanBH <mcleanbh@gmail.com>
2015-03-13 20:39:41 -0400

    trying to implement weather layer of google maps API

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -8,10 +8,32 @@ export default Ember.Component.extend({
         var options = {
             center: new window.google.maps.LatLng(
                 this.get('latitude'),
-                this.get('longitude')
+                this.get('longitude'),
+                this.get('cloudLayer')
             ),
             zoom: 15
         };
         new window.google.maps.Map(container, options);
     }.on('didInsertElement')
 });
+
+
+// function initialize() {
+// var mapOptions = {
+// zoom: 4,
+// center: new google.maps.LatLng(49.265984,-123.127491)
+// };
+//
+// var map = new google.maps.Map(document.getElementById('map-canvas'),
+//   mapOptions);
+//
+// var weatherLayer = new google.maps.weather.WeatherLayer({
+// temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
+// });
+// weatherLayer.setMap(map);
+//
+// var cloudLayer = new google.maps.weather.CloudLayer();
+// cloudLayer.setMap(map);
+// google.maps.event.addDomListener(window, 'load', initialize);
+// }
+// });

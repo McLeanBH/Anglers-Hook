010028237c944147abc764e519f041cdbdc641b4
McLeanBH <mcleanbh@gmail.com>
2015-03-14 22:09:12 -0400

    images filled in media gallery, wu logo for 'powered by' credit

--- app/adapters/weather-radar.js
+++ app/adapters/weather-radar.js
@@ -7,7 +7,28 @@ export default Ember.Object.extend({
       return ajax("http://api.wunderground.com/api/" + id + ".json").then(function(result){
         return result.data;
       });
-    }
+    },
+
+  findAll: function(){
+      return ajax("http://api.wunderground.com/api/").then(function(response){
+        return response.results.map(function(){
+          return ;
+        });
+      });
+    },
+
+  findQuery: function(name, query) {
+    return ajax("http://api.wunderground.com/api/", {
+      data: Ember.$.param({
+          where: JSON.stringify(query)
+        })
+    }).then(function(response){
+      return response.results.map(function(){
+        return;
+      });
+    });
+  }
 });
 
-// "http://api.wunderground.com/api/d98a9b35b6b3cbd9/animatedradar/animatedsatellite/geolookup/forecast/conditions/hourly/rawtide/tide/bestfct/q/autoip.gif.json"
+// "http://api.wunderground.com/api/d98a9b35b6b3cbd9/animatedradar/...
+// ... animatedsatellite/geolookup/forecast/conditions/hourly/rawtide/tide/bestfct/q/autoip.gif.json"

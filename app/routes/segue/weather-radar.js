c73bfc3921129b077a05da33b4da7ec6c78fa38a
McLeanBH <mcleanbh@gmail.com>
2015-03-16 14:58:08 -0400

    added wunderground api to CSP

--- app/routes/segue/weather-radar.js
+++ app/routes/segue/weather-radar.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
+  model: function(params){
     return this.store.findQuery('weather-radar');
+    // return this.store.findQuery('weather-radar', params.current_observation.wind_string);
   },
 });

a746262f5177bccceeaa118d09a62513d863de55
McLeanBH <mcleanbh@gmail.com>
2015-03-18 15:15:27 -0400

    fixed radar/satellite size, and now data corresponds to radar location

--- app/adapters/weather-data.js
+++ app/adapters/weather-data.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Object.extend({
   findQuery: function(name, query){
-    return ajax("http://api.wunderground.com/api/92a39adfcd902ac7/animatedradar/animatedsatellite/geolookup/conditions/forecast/hourly/rawtide/tide/q/zmw:29401.1.99999.json", {
+    return ajax("http://api.wunderground.com/api/92a39adfcd902ac7/animatedradar/animatedsatellite/geolookup/conditions/forecast/hourly/rawtide/tide/q/autoip.json", {
       dataType: 'jsonp'
     }).then(function(response){
       console.log(response);

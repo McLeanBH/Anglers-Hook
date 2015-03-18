278e11c791c3987a2d82a6a420595856334929e6
McLeanBH <mcleanbh@gmail.com>
2015-03-18 17:39:54 -0400

    loading route now working, updated styling to be responsive

--- app/controllers/segue/weather-radar.js
+++ app/controllers/segue/weather-radar.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  radarUrl: function(){
-    var location = "autoip";
-    return "http://api.wunderground.com/api/92a39adfcd902ac7/animatedradar/animatedsatellite/q/" + location + ".gif?num=8&radius=1000&delay=25&rad.maxlat=32.7833&rad.maxlon=79.9333&rad.minlat=31.596&rad.minlon=97.388&sat.maxlat=47.709&sat.maxlon=-69.263&sat.minlat=31.596&sat.minlon=-97.388&interval=30&rad.width=450$rad.height=450&rad.rainsnow=1&rad.reproj.automerc=1&rad.num=5&sat.width=450&sat.height=450&sat.key=sat_ir4&sat.gtt=107&sat.proj=me&sat.timelabel=1&sat.num=5&borders=0";
-  }.property()
+  // radarUrl: function(){
+  //   var location = "autoip";
+  //   return "http://api.wunderground.com/api/92a39adfcd902ac7/animatedradar/animatedsatellite/q/" + location + ".gif?num=8&radius=1000&delay=25&rad.maxlat=32.7833&rad.maxlon=79.9333&rad.minlat=31.596&rad.minlon=97.388&sat.maxlat=47.709&sat.maxlon=-69.263&sat.minlat=31.596&sat.minlon=-97.388&interval=30&rad.width=450$rad.height=450&rad.rainsnow=1&rad.reproj.automerc=1&rad.num=5&sat.width=450&sat.height=450&sat.key=sat_ir4&sat.gtt=107&sat.proj=me&sat.timelabel=1&sat.num=5&borders=0";
+  // }.property()
 });
 
 
--- app/routes/segue/weather-radar.js
+++ app/routes/segue/weather-radar.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return this.store.findQuery('weather-data');
-  },
+    return Ember.RSVP.hash({
+      data: this.store.findQuery('weather-data', "autoip"),
+      radar: this.store.findQuery('weather-radar', 'autoip')
+    });
+  }
 });
 
 

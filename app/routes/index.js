3eabe0d0915a9e42701c69fcf3954f54c2f5d282
McLeanBH <mcleanbh@gmail.com>
2015-03-19 19:54:07 -0400

    trying to debug weather-radar adapter

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(){
-    return this.store.findAll('radar', 'user');
+    return this.store.findAll('radar');
   }
 });

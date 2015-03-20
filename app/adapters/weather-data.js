1e60178a48247beb6aa77fd9cda350869feea9a5
McLeanBH <mcleanbh@gmail.com>
2015-03-20 12:32:05 -0400

    updated weather stats, styled radar

--- app/adapters/weather-data.js
+++ app/adapters/weather-data.js
@@ -3,9 +3,10 @@ import Ember from 'ember';
 
 export default Ember.Object.extend({
   findQuery: function(name, query){
-    return ajax("http://api.wunderground.com/api/92a39adfcd902ac7/animatedradar/animatedsatellite/geolookup/conditions/forecast/hourly/rawtide/tide/q/autoip.json", {
+    return ajax("http://api.wunderground.com/api/92a39adfcd902ac7/animatedradar/animatedsatellite/geolookup/conditions/forecast/hourly/rawtide/tide/q/29401.json", {
       dataType: 'jsonp'
     }).then(function(response){
+      console.log(response);
       return response;
     });
   }

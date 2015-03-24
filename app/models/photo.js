51a651bedf01ae713fa95bcf4504a6646f3983e2
McLeanBH <mcleanbh@gmail.com>
2015-03-24 16:39:33 -0400

    finagling

--- app/models/photo.js
+++ app/models/photo.js
@@ -2,7 +2,7 @@ import ajax from 'ic-ajax';
 import Model from 'ember-magic-man/model';
 
 export default Model.extend({
-  addFavorite: function(photos) {
+  addFavorite: function(photo) {
     return ajax("https://api.parse.com/1/photos/" + this.id, {
       type: "PUT",
       data: JSON.stringify({
@@ -12,7 +12,7 @@ export default Model.extend({
             {
               __type: 'Pointer',
               className: 'Photo',
-              objectId: bookmark.id
+              objectId: photo.id
             }
           ]
         }
@@ -20,36 +20,3 @@ export default Model.extend({
     });
   }
 });
-
-
-
-// export default Model.extend({
-//   toJSON: function(){
-//     var data = this._super();
-//     var userId = this.get('createdBy.id');
-//     if(userId) {
-//       data.set('createdBy', { __type: 'Pointer', className: '_User', objectId: userId }); }
-//     return data;
-//   }
-// });
-
-
-// export default Model.extend({
-//   addFavorite: function(bookmark) {
-//     return ajax("https://api.parse.com/1/users/" + this.id, {
-//       type: "PUT",
-//       data: JSON.stringify({
-//         favorites: {
-//           __op: "AddRelation",
-//           objects: [
-//             {
-//               __type: 'Pointer',
-//               className: 'Bookmark',
-//               objectId: bookmark.id
-//             }
-//           ]
-//         }
-//       })
-//     });
-//   }
-// });

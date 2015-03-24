60fc93e56204e2ad417a59a6f8b613455b47527b
McLeanBH <mcleanbh@gmail.com>
2015-03-24 09:41:03 -0400

    attempted updated at register controller

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,14 +3,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    saveUser: function(){
-      var userData = this.getProperties('firstName', 'lastName', 'email', 'username', 'password');
-      var self = this;
-      this.get('model').setProperties(userData);
-      this.get('model').save().then(function(user){
-        self.get('session').authenticate('authenticator:parse-email', user);
-      });
+//   actions: {
+//     saveUser: function(){
+//       var userData = this.getProperties('firstName', 'lastName', 'email', 'username', 'password');
+//       var self = this;
+//       this.get('model').setProperties(userData);
+//       this.get('model').save().then(function(user){
+//         self.get('session').authenticate('authenticator:parse-email', user);
+//       });
+//     }
+//   }
+// });
+
+    actions: {
+      save: function() {
+        var self = this;
+        var user = this.get('model');
+        user.username = user.email;
+        user.save().then(function(){
+          self.get('session').authenticate('authenticator:parse-email', user);
+        });
+        this.transitionToRoute('dashboard');
+      }
     }
-  }
-});
+  });

8d49f865491c72542a1fad2af4f31d4652d5bf98
McLeanBH <mcleanbh@gmail.com>
2015-03-24 11:29:01 -0400

    Fixed registration specifically, referenced the model in registration.hbs, updated authenticator newest version of magic man installed

--- app/routes/register.js
+++ app/routes/register.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return this.store.createRecord('user', {
-      createdBy: this.get('session.currentUser')
-    });
+    return this.store.createRecord('user');
   },
   actions: {
     createRecord: function(){

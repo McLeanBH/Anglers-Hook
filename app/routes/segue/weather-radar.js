e79cb5cdd2a35a004f1eff4a9c312c97482a217e
McLeanBH <mcleanbh@gmail.com>
2015-03-27 12:28:01 -0400

    updated weather-room to include search

--- app/routes/segue/weather-radar.js
+++ app/routes/segue/weather-radar.js
@@ -6,5 +6,17 @@ export default Ember.Route.extend({
       data: this.store.findQuery('weather-data', "autoip"),
       radar: this.store.findQuery('weather-radar', 'autoip')
     });
+  },
+
+  actions: {
+    search: function(){
+      return Ember.RSVP.hash({
+        data: this.store.findQuery('weather-data', "29401"),
+        radar: this.store.findQuery('weather-radar', '29401')
+      }).then(function(results){
+        console.log('results', results);
+        this.get('controller').set('model', results);
+      }.bind(this));
+    }
   }
 });
